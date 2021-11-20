8e620d23a767ec0f911a0f9d122d11b4fc68f5e8
Arush Mehrotra <43364437+arush-mehrotra@users.noreply.github.com>
2021-11-20 15:27:28 -0500

    about-page-update

--- src/components/About.js
+++ src/components/About.js
@@ -3,13 +3,41 @@ import { userContext } from '../App'
 
 const About = () => {
     return (
-        <div style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-        }}>
-            <h1>Our Purpose</h1>
-        </div>
+        <div class = "about-container">
+            <div class = "desktop-11">
+                <div class = "our-purpose">
+                    <h1>Our Purpose</h1>
+                    <p>A tech-enabled nonprofit empowering students with the knowledge to manage their money and reach financial independence</p>
+                </div>
+                <div class = "purpose-how1">
+                    <p> We are redefining financial education through... </p>
+                </div>
+                <div class = "flex-container">
+                    <div class = "purpose-how2">
+                        <p>Campus Chapters</p>
+                    </div>
+                    <div class = "purpose-how2-image">
+                        <p>Placeholder</p>
+                    </div>
+                </div>
+                <div class = "flex-container">
+                    <div class = "purpose-how2-image">
+                        <p>Placeholder</p>
+                    </div>
+                    <div class = "purpose-how2">
+                        <p>Product Suggestions</p>
+                    </div>
+                </div>
+                <div class = "flex-container">
+                    <div class = "purpose-how2">
+                        <p>User-Centric Resources</p>
+                    </div>
+                    <div class = "purpose-how2-image">
+                        <p>Placeholder</p>
+                    </div>
+                </div>
+            </div>
+        </div>    
     )
 }
 
